ed1d6fff39fe7b88ad6a35b4e4c6a08e6ac892a8
JoshuaAdenusi-Prince <joshua.adenusi-prince@cdpipelinedevshop.com>
2025-01-27 19:01:11 -0500

    Update CatTest.java

--- src/test/java/com/codedifferently/labs/partB/CatTest.java
+++ src/test/java/com/codedifferently/labs/partB/CatTest.java
@@ -1,4 +1,13 @@
 package com.codedifferently.labs.partB;
 
+import partB.animals.Cat;
+import partB.animals.Mammal;
+
 public class CatTest {
+    public static void main(String[] args) {
+       Cat ginger = new Cat();
+       ginger.setName("Garfield");
+
+    }
+
 }
