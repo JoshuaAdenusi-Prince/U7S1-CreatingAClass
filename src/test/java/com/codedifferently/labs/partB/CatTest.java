c085778d811fdfd985b5bfbd052da4c36b00c6be
JoshuaAdenusi-Prince <joshua.adenusi-prince@cdpipelinedevshop.com>
2025-01-29 19:01:00 -0500

    Update CatTest.java

--- src/test/java/com/codedifferently/labs/partB/CatTest.java
+++ src/test/java/com/codedifferently/labs/partB/CatTest.java
@@ -1,13 +1,34 @@
 package com.codedifferently.labs.partB;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 import partB.animals.Cat;
 import partB.animals.Mammal;
+import partB.food.Food;
+
+import java.util.Date;
 
 public class CatTest {
-    public static void main(String[] args) {
-       Cat ginger = new Cat();
-       ginger.setName("Garfield");
+    @Test
+    void testSetName(){
+        Date bDay = new Date();
+        Cat ginger = new Cat("Garfield",bDay,11212);
 
+        ginger.setName("Garfield");
+        Assertions.assertEquals("Garfield",ginger.getName());
+
+        ginger.setBirthDate(bDay);
+        Assertions.assertEquals(bDay,ginger.getBirthDate());
+
+        ginger.speak();
+        Assertions.assertEquals("meow!",ginger.speak());
+
+        Food fish = new Food();
+        ginger.eat(fish);
+        Assertions.assertEquals(1,ginger.getNumberOfMealsEaten());
+
+        ginger.getId();
+        Assertions.assertEquals(1,ginger.getId());
     }
 
 }
