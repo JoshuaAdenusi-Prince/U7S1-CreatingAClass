a02bbfb23e026c26233a937df9b56b00be1588c8
JoshuaAdenusi-Prince <joshua.adenusi-prince@cdpipelinedevshop.com>
2025-02-07 18:58:06 -0500

    Update CatHouseTest.java

--- src/test/java/com/codedifferently/labs/partB/CatHouseTest.java
+++ src/test/java/com/codedifferently/labs/partB/CatHouseTest.java
@@ -1,5 +1,6 @@
 package com.codedifferently.labs.partB;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import partB.animals.Cat;
 import partB.storage.CatHouse;
@@ -9,6 +10,11 @@ import java.util.Date;
 public class CatHouseTest {
     @Test
     void addCatTest(){
+        Cat pookie = new Cat("jax", new Date(), 22438);
+        CatHouse.add(pookie);
+        Assertions.assertEquals("jax",pookie.getName());
+        
+
 //        String cat = "corithin";
 //        Cat coritheon = new Cat("coritheon", new Date(), 13977);
 //        Cat pantheon = new Cat("pantheon", new Date(), 677139);
